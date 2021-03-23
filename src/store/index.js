fe8d9bcdf95d0b0092ae126dd46da4f57942ba30
Angel <angelmanuelmartinezfernandez@gmail.com>
2021-03-23 13:19:55 +0100

    merge store hecho

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,4 @@
-import { createStore } from 'vuex'
+import {createStore} from 'vuex'
 
 export default createStore({
   state: {
@@ -9,16 +9,39 @@ export default createStore({
       email: '',
       pass: ''
     },
+    tasks: [
+      {
+        name: 'Nombre de la tarea',
+        description: 'Descripción o consejos de la tarea'
+      }
+    ],
     task: {
       person: '',
       taskName: '',
       taskCategory: ''
-    }
+    },
+    notes: [
+      {
+        title: 'Nota 1',
+        noteContent: 'Contenido de la nota'
+      },
+      {
+        title: 'Nota 2',
+        noteContent: 'Contenido de la nota'
+      },
+      {
+        title: 'Nota 2',
+        noteContent: 'Contenido de la nota'
+      },
+
+    ],
+    note: {
+      name: '',
+      noteContent: ''
+    },
+    weekDays: ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo']
   },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+  mutations: {},
+  actions: {},
+  modules: {}
 })
