14308d4a4ceb9ebbcae2151fc010a5a4ad36a4e3
Angel <angelmanuelmartinezfernandez@gmail.com>
2021-03-23 14:05:39 +0100

    ordenada navbar por trabajo hecho

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <div id="nav" class="d-flex justify-content-center">
     <router-link to="/start">Start</router-link> |
-    <router-link to="/">Home</router-link> |
     <router-link to="/register">Register</router-link> |
-    <router-link to="/login">Login</router-link> |
     <router-link to="/profile">Profile</router-link> |
+    <router-link to="/">Home</router-link> |
+    <router-link to="/login">Login</router-link> |
     <router-link to="/new-task">Task</router-link> |
   </div>
   <router-view/>
