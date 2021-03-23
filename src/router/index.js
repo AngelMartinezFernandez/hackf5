b13cbd888e01b650c977ac08e9daffee6f3c8308
MAlexGG <alexandragalarzag@gmail.com>
2021-03-23 12:04:22 +0100

    start

--- src/router/index.js
+++ src/router/index.js
@@ -1,36 +1,47 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: () => import('../views/Home.vue')  },
-  {
-    path: '/register',
-    name: 'Register',
-    component: () => import('../views/Register.vue')
-  },
-  {
-    path: '/login',
-    name: 'Login',
-    component: () => import('../views/Login.vue')
-  },
-  {
-    path: '/profile',
-    name: 'Profile',
-    component: () => import('../views/Profile.vue')
-  },
-  {
-    path: '/new-task',
-    name: 'FormTask',
-    component: () => import('../views/FormTask.vue')
-  }
+const routes = [{
+        path: '/start',
+        name: 'Start',
+        component: () =>
+            import ('../views/Start.vue')
+    },
+    {
+        path: '/',
+        name: 'Home',
+        component: () =>
+            import ('../views/Home.vue')
+    },
+    {
+        path: '/register',
+        name: 'Register',
+        component: () =>
+            import ('../views/Register.vue')
+    },
+    {
+        path: '/login',
+        name: 'Login',
+        component: () =>
+            import ('../views/Login.vue')
+    },
+    {
+        path: '/profile',
+        name: 'Profile',
+        component: () =>
+            import ('../views/Profile.vue')
+    },
+    {
+        path: '/new-task',
+        name: 'FormTask',
+        component: () =>
+            import ('../views/FormTask.vue')
+    }
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+    history: createWebHistory(process.env.BASE_URL),
+    routes
 })
 
 export default router
---
+++ src/views/Start.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="start">
+    
+    <img src="/imgs/animal-logo.png" class="logo-start">
+
+  </div>
+</template>
+
+
+<style scoped>
+
+  .start {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .logo-start{
+    width: 90vw;
+  }
+
+</style>
